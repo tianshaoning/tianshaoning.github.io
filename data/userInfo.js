518b24064764338644c853f8dc7ed496a822500b
tianshaoning <1403049031@qq.com>
2016-10-25 18:48:21 +0800

    add personal cv

---
+++ data/userInfo.js
@@ -0,0 +1,35 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "田绍宁" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1403049031",
+    userEmail:   "1403049031@qq.com",
+    motto:  "一个从不犯错误的人，一定从来没有尝试过任何新鲜事物。——艾尔伯特·爱因斯坦", 
+    userPhone: 15819194506,
+    userOrignPlace:  "河北省邢台市" ,
+    userSeatPlace:  "北京市",
+    userAssessment: "个人工作态度端正，有良好进取心，同时自学能力强，能很快适应新的环境，为人和善",   
+    userHobby: ["运动","电影","游戏"],
+}
+
